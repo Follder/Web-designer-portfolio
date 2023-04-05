2dcbc470df7b1bb409dec0afe479d294778a9bad
DKoriahin <Dkoryahin@gmail.com>
2023-04-05 10:58:28 +0300

    full project

---
+++ dist/js/script.js
@@ -0,0 +1,24 @@
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close'),
+    closeOverlay = document.querySelector('.menu__overlay');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+function closeMenu(elem) {
+    elem.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+}
+
+closeMenu(closeElem);
+closeMenu(closeOverlay);
+
+const skillsNum = document.querySelectorAll('.skills-card__num'),
+    skillsLine = document.querySelectorAll('.skills-card__line_full');
+
+    skillsNum.forEach((item, i) => {
+        skillsLine[i].style.width = item.textContent;
+    });
