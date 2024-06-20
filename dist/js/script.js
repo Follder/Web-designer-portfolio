31b706a70ca99c24f2d5d90d42750a43f23bfc58
Dmytro Koriahin <Dkoryahin@gmail.com>
2024-06-21 00:29:57 +0300

    add animation and readme.md

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,24 +1,26 @@
-const hamburger = document.querySelector('.hamburger'),
-    menu = document.querySelector('.menu'),
-    closeElem = document.querySelector('.menu__close'),
-    closeOverlay = document.querySelector('.menu__overlay');
+AOS.init();
 
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
+const hamburger = document.querySelector(".hamburger"),
+  menu = document.querySelector(".menu"),
+  closeElem = document.querySelector(".menu__close"),
+  closeOverlay = document.querySelector(".menu__overlay");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.add("active");
 });
 
 function closeMenu(elem) {
-    elem.addEventListener('click', () => {
-        menu.classList.remove('active');
-    });
+  elem.addEventListener("click", () => {
+    menu.classList.remove("active");
+  });
 }
 
 closeMenu(closeElem);
 closeMenu(closeOverlay);
 
-const skillsNum = document.querySelectorAll('.skills-card__num'),
-    skillsLine = document.querySelectorAll('.skills-card__line_full');
+const skillsNum = document.querySelectorAll(".skills-card__num"),
+  skillsLine = document.querySelectorAll(".skills-card__line_full");
 
-    skillsNum.forEach((item, i) => {
-        skillsLine[i].style.width = item.textContent;
-    });
+skillsNum.forEach((item, i) => {
+  skillsLine[i].style.width = item.textContent;
+});
